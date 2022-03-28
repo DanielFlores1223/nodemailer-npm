b07dad7e6d92a79d716cf916668664d0ac093ddd
DanielFlores1223 <dany-232000@hotmail.com>
2022-03-28 10:26:19 -0600

    Readme.md added

--- controllers/nodemailer.controller.js
+++ controllers/nodemailer.controller.js
@@ -3,13 +3,22 @@ const { credentials } = require('../config/credentials');
 
 
 /*
-
+     Está función permite enviar un email, mediante los parametros que se le pasen en el req.body
+     @subject : es el asunto del email
+     @message  : Mensaje del email
+     @to : Destinatario del email
 */
 const sendEmail = (req, res) => {
      const { subject, message, to } = req.body;
 
      const { email, password, service } = credentials;
      
+     /* Crear un transportador reusable, el cual es un objeto que permite enviar el mail
+        service es el servicio de correo que necesita la cuenta
+        auth: 
+                user: es el correo de la cuenta
+                password: es la contraseña de la cuenta
+     */  
      const transporter = nodemailer.createTransport({
           service,
           auth: {
@@ -17,14 +26,16 @@ const sendEmail = (req, res) => {
             pass: password
           }
         });
-        //
+
+     //Definincion del destinatario, asunto y mensaje 
      const mailOptions = {
           from: email,
           to,
           subject,
           text: message
         };
-        //
+
+     //Función que envia el email
      transporter.sendMail(mailOptions, function(error, info){
        if (error) {
          console.log(error);
@@ -37,7 +48,7 @@ const sendEmail = (req, res) => {
 
 }
 
-
+// se exporta la funcion de enviar email
 module.exports = {
      sendEmail
 }
--- routes/nodemailer.route.js
+++ routes/nodemailer.route.js
@@ -3,6 +3,12 @@ const router = express.Router();
 const controller = require('../controllers/nodemailer.controller')
 const path = 'nodemailer'
 
+/*
+ Para enviar un email necesitas acceder a esta ruta y manderle mediante el request body 
+ subject: es el asunto del email
+ message: Mensaje del email
+ to: Destinatario del email
+*/
 router.post(`/${path}`, controller.sendEmail);
 
 module.exports = router;
